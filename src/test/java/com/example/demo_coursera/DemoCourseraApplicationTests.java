ec7805f125edcd555a047b1d2e72ff48ea9edddf
Olga <flighter@mail.ru>
2021-08-07 14:42:10 +0300

    initial commit

---
+++ src/test/java/com/example/demo_coursera/DemoCourseraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_coursera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCourseraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
